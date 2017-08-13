0678386426341442456cd31bb5d716ff1c1d50e5
Ivanova Diana <ivanova.dina.de@gmail.com>
2017-08-13 22:02:27 +0200

    configuring local postgresql

--- src/main/java/fratcher/Application.java
+++ src/main/java/fratcher/Application.java
@@ -2,9 +2,11 @@ package fratcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/fratcher/user/User.java
@@ -0,0 +1,71 @@
+package fratcher.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "User_")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    @JsonIgnore
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        return id != null ? id.equals(user.id) : user.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
